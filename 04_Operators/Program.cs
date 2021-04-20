1dc77a41d95aa629ec00afbf6c5eb729b18020ab
Chris Kronberg <ckronber@gmail.com>
2021-04-19 21:49:26 -0400

    Operators

---
+++ 04_Operators/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _04_Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(5 + 2);
+            Console.WriteLine(10 - 6);
+            Console.WriteLine(2 * 3);
+            Console.WriteLine(10 / 2);
+
+            Console.WriteLine(10 % 2);
+            Console.WriteLine(10 % 9);
+            Console.WriteLine(16 % 3);
+            Console.WriteLine(19 % 5);
+
+            Console.WriteLine(10 % 3);
+            Console.WriteLine(9 % 2);
+            Console.WriteLine(100 % 22);
+            Console.WriteLine(13 % 12);
+        }
+    }
+}
