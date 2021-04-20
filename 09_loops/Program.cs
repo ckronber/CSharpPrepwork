fa872ae71c2b60cb43b67f3e0205b43896e41a26
Chris Kronberg <ckronber@gmail.com>
2021-04-19 23:36:21 -0400

    loops

---
+++ 09_loops/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _09_loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // While Loop
+            //1
+            int number = 0;
+            //2
+            while(number <= 100)
+            {
+                //3
+                Console.WriteLine(number);
+                number += 5;
+                //4
+            }
+            //5
+            Console.ReadLine();
+
+            //for loop
+            number = 100;
+
+            for(int i = 1; i <= number; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
