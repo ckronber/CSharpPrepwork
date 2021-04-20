0387811633f8dc9c29b7456f2a97207a92ca5055
Chris Kronberg <ckronber@gmail.com>
2021-04-19 22:28:20 -0400

    Boolean Challenges

---
+++ 06_boolean_challenges/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _06_boolean_challenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bronze and Silver
+            float value = 101/2.0f;
+           // Console.WriteLine(value);
+            if (value == 50.0)
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+
+            //Gold
+            int randVal = 29;
+            Console.WriteLine("Pick a number between 1 and 100: ");
+            int userVal = Convert.ToInt32(Console.ReadLine());
+
+            if(userVal == randVal)
+            {
+                Console.WriteLine("Nice Job! You guessed correctly.");
+            }
+            else if(userVal > randVal)
+            {
+                Console.WriteLine("Choose a lower number.");
+            }
+            else
+            {
+                Console.WriteLine("Choose a higher number.");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
