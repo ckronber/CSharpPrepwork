0ab5fc62175600938bc84b0ec03b4bd0177eb631
Chris Kronberg <ckronber@gmail.com>
2021-04-19 20:22:03 -0400

    Variable1

---
+++ 02_variables/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _02_variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1     //2         //3
+            string firstName = "Paul";
+            Console.WriteLine(firstName);
+            Console.ReadLine();
+        }
+    }
+}
