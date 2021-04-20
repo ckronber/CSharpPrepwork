5bd3f0c9e00236fc6c9c0256329aa6eb3b0f8f40
Chris Kronberg <ckronber@gmail.com>
2021-04-19 22:09:49 -0400

    Boolean

---
+++ 06_booleans/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _06_booleans
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = 100 / 2;
+            //1
+            if (value == 50)
+            {
+                //2
+                Console.WriteLine(true);
+            }
+
+            //3
+            Console.WriteLine("Since I'm not in the if block, I'll always display.");
+        }
+    }
+}
