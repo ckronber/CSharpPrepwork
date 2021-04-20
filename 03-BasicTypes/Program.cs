e1ae9d4c77a03476e71000cb73a1ceb6f25b2deb
Chris Kronberg <ckronber@gmail.com>
2021-04-19 20:52:06 -0400

    data types

---
+++ 03-BasicTypes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _03_BasicTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isLoggedIn = true; 
+            Console.WriteLine(isLoggedIn);
+
+            decimal accountBalance = 80000.30m;
+            Console.WriteLine(accountBalance);
+
+            Console.ReadLine();
+        }
+    }
+}
