1de5ff0ddf6ebea744fbf475723f382cb0ebc928
Chris Kronberg <ckronber@gmail.com>
2021-04-19 23:48:02 -0400

    Loops Challenge

---
+++ 09_Loops_Challenge/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _09_Loops_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bronze
+            for (int i = 500; i < 526; i++)
+                Console.WriteLine(i);
+
+            //Silver
+            for (int i = 1; i < 101; i++)
+                Console.WriteLine(i);
+
+            //Gold
+            for (int i = 1; i < 101; i++)
+            {
+                if ((i % 3) == 0 && (i % 5) == 0)
+                    Console.WriteLine("FizzBuzz");
+                else if (i % 3 == 0)
+                    Console.WriteLine("Fizz");
+                else if (i % 5 == 0)
+                    Console.WriteLine("Buzz");
+             }
+        }
+
+    }
+}
