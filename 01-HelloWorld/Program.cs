4d3cf9f1832bc22805ee1e9331f219dea776d877
Chris Kronberg <ckronber@gmail.com>
2021-04-19 20:15:18 -0400

    Add project files.

---
+++ 01-HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _01_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hey What's up?");
+            Console.WriteLine("Not much how about with you?");
+            Console.WriteLine("Not much, have a great day!");
+            Console.ReadLine();
+        }
+    }
+}
